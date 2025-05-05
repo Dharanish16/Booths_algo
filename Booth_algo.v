6ec5be825132bef61351d3ebabbfd26314d50a82
Dharanish16 <161188989+Dharanish16@users.noreply.github.com>
2025-05-05 12:48:16 +0530

    Booth_algo.v

--- Booth_algo.v
+++ Booth_algo.v
@@ -20,76 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-/*module Booth_algo(clk,rst,start,X,Y,valid,Z);
-    input clk,rst,start;
-    input signed [3:0]X,Y; 
-    output reg signed [7:0]Z; //carries the product of multiplicand and multiplier
-    output valid;
-    
-    reg [7:0]next_Z,Z_temp;
-    reg ns,ps;
-    reg [1:0]count,next_count;
-    reg [1:0]temp,next_temp;
-    reg valid,next_valid;
-    
-    parameter IDLE = 1'b0;
-    parameter START = 1'b1;
-    
-    always @(posedge clk or negedge rst)
-        begin   
-            if(!rst)
-                begin   
-                    Z <= 8'd0;
-                    valid <= 1'b0;
-                    ps <= 1'b0;
-                    temp <= 2'd0;
-                    count <= 2'd0;
-                end
-             else
-                begin
-                    Z <= next_Z;
-                    valid <= next_valid;
-                    ps <= ns;
-                    temp <= next_temp;
-                    count <= next_count;
-                end
-       end
-       always @(*)
-            begin
-                case(ps)
-                    IDLE :  begin
-                                next_count = 2'b0;
-                                next_valid = 1'b0;
-                                if(start)
-                                    begin
-                                        ns = START;
-                                        next_temp = {X[0],1'b0};
-                                        next_Z = {4'd0,X};
-                                    end 
-                                else
-                                    begin
-                                        ns = ps;
-                                        next_temp = 2'd0;
-                                        next_Z = 8'd0; 
-                                     end 
-                             end
-                      START : begin 
-                                case(temp)
-                                    2'b10 : Z_temp = {(Z[7:4]-Y),Z[3:0]};
-                                    2'b01 : Z_temp = {(Z[7:4]+Y),Z[3:0]};
-                                    default : Z_temp = {Z[7:4],Z[3:0]};
-                                endcase
-                                
-                                next_temp = {X[count+1],X[count]};
-                                next_count = count + 1'b1;
-                                next_Z = Z_temp >>> 1;
-                                next_valid = (&count) ? 1'b1 : 1'b0;
-                                ns = (&count) ? IDLE : ps;
-                             end 
-                  endcase 
-              end                 
-endmodule
-*/
+
 module BoothMul(clk,rst,start,X,Y,valid,Z);
 
 input clk;
@@ -165,3 +96,74 @@ end
 endcase
 end
 endmodule
+
+/*module Booth_algo(clk,rst,start,X,Y,valid,Z);
+    input clk,rst,start;
+    input signed [3:0]X,Y; 
+    output reg signed [7:0]Z; //carries the product of multiplicand and multiplier
+    output valid;
+    
+    reg [7:0]next_Z,Z_temp;
+    reg ns,ps;
+    reg [1:0]count,next_count;
+    reg [1:0]temp,next_temp;
+    reg valid,next_valid;
+    
+    parameter IDLE = 1'b0;
+    parameter START = 1'b1;
+    
+    always @(posedge clk or negedge rst)
+        begin   
+            if(!rst)
+                begin   
+                    Z <= 8'd0;
+                    valid <= 1'b0;
+                    ps <= 1'b0;
+                    temp <= 2'd0;
+                    count <= 2'd0;
+                end
+             else
+                begin
+                    Z <= next_Z;
+                    valid <= next_valid;
+                    ps <= ns;
+                    temp <= next_temp;
+                    count <= next_count;
+                end
+       end
+       always @(*)
+            begin
+                case(ps)
+                    IDLE :  begin
+                                next_count = 2'b0;
+                                next_valid = 1'b0;
+                                if(start)
+                                    begin
+                                        ns = START;
+                                        next_temp = {X[0],1'b0};
+                                        next_Z = {4'd0,X};
+                                    end 
+                                else
+                                    begin
+                                        ns = ps;
+                                        next_temp = 2'd0;
+                                        next_Z = 8'd0; 
+                                     end 
+                             end
+                      START : begin 
+                                case(temp)
+                                    2'b10 : Z_temp = {(Z[7:4]-Y),Z[3:0]};
+                                    2'b01 : Z_temp = {(Z[7:4]+Y),Z[3:0]};
+                                    default : Z_temp = {Z[7:4],Z[3:0]};
+                                endcase
+                                
+                                next_temp = {X[count+1],X[count]};
+                                next_count = count + 1'b1;
+                                next_Z = Z_temp >>> 1;
+                                next_valid = (&count) ? 1'b1 : 1'b0;
+                                ns = (&count) ? IDLE : ps;
+                             end 
+                  endcase 
+              end                 
+endmodule
+*/
