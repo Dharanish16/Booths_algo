f1cf321cd7ec2f5bd4845cda0bd0c407f955a370
Dharanish16 <161188989+Dharanish16@users.noreply.github.com>
2025-05-05 12:48:43 +0530

    booth_tb.v

--- booth_tb.v
+++ booth_tb.v
@@ -20,30 +20,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-/*module Booth_algo_tb();
-    reg clk,rst,start;
-    reg signed [3:0]X,Y;
-    wire signed [7:0]Z;
-    wire valid;
 
-always #5 clk = ~clk;
-
-Booth_algo DUT(clk,rst,start,X,Y,valid,Z);
-
-initial
-$monitor($time,"X=%d, Y=%d, valid=%d, Z=%d ",X,Y,valid,Z);
-initial
-begin
-X=5;Y=7;clk=1'b1;rst=1'b0;start=1'b0;
-#10 rst = 1'b1;
-#10 start = 1'b1;
-#10 start = 1'b0;
-@valid
-#10 X=-4;Y=6;start = 1'b1;
-#10 start = 1'b0;
-$finish;
-end 
-endmodule*/
 module booth_tb;
 
 reg clk,rst,start;
@@ -68,3 +45,28 @@ X=5;Y=7;clk=1'b1;rst=1'b0;start=1'b0;
 #10 start = 1'b0;
 end      
 endmodule
+
+/*module Booth_algo_tb();
+    reg clk,rst,start;
+    reg signed [3:0]X,Y;
+    wire signed [7:0]Z;
+    wire valid;
+
+always #5 clk = ~clk;
+
+Booth_algo DUT(clk,rst,start,X,Y,valid,Z);
+
+initial
+$monitor($time,"X=%d, Y=%d, valid=%d, Z=%d ",X,Y,valid,Z);
+initial
+begin
+X=5;Y=7;clk=1'b1;rst=1'b0;start=1'b0;
+#10 rst = 1'b1;
+#10 start = 1'b1;
+#10 start = 1'b0;
+@valid
+#10 X=-4;Y=6;start = 1'b1;
+#10 start = 1'b0;
+$finish;
+end 
+endmodule*/
